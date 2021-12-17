c7a6f119380cfb918bdd55db349823b6a891b184
CodeMD96 <codemd96@gmail.com>
2021-12-17 15:52:10 +0000

    update function error message when movie not found

--- src/movie/movie.function.js
+++ src/movie/movie.function.js
@@ -36,12 +36,16 @@ exports.readMovie = async (movieObj) => {
 
 exports.updateMovie = async (updateObj) => {
     try {
-        let doc = await Movie.find({title: updateObj.title});
-        doc= doc[0];
-        doc[updateObj.updateKey] = await updateObj.updateValue;
-        await doc.save();
-        mongoose.disconnect();
-        console.log("Successfully updated")
+        let movie = await Movie.find({title: updateObj.title});
+        if (movie[0]) {
+            movie[0][updateObj.updateKey] = await updateObj.updateValue;
+            await movie[0].save();
+            mongoose.disconnect();
+            console.log("Successfully updated");
+        } else {
+            mongoose.disconnect();
+            console.log("Movie doesn't exist");
+        }
     } catch (error) {
         console.log(error);
         mongoose.disconnect();
@@ -52,10 +56,10 @@ exports.deleteMovie = async (movieObj) => {
     try {
         const movie = await Movie.find( {title: movieObj.title} );
         if (movie[0]) {
-            await Movie.deleteOne({title: movieObj.title})
-            console.log("Successfully deleted")
+            await Movie.deleteOne({title: movieObj.title});
+            console.log("Successfully deleted");
         } else {
-            console.log("Movie doesn't exist")
+            console.log("Movie doesn't exist");
         }
         mongoose.disconnect();
     } catch (error) {
